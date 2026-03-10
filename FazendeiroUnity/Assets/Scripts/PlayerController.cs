e8d874d5bd3226b3f37a03ef2441aa90befd0695
baracle67 <avalugggeludo@gmail.com>
2026-03-10 17:08:11 -0300

    versao incial

---
+++ FazendeiroUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20f;
+    public float xRange = 15f;
+    public GameObject projectilePrefab;
+    private float horizontalInput;
+    public InputActionAsset InputActions;
+    private InputAction moveAction;
+    private InputAction fireAction;
+    private InputAction pauseAction;
+    private InputAction despauseAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = moveAction.ReadValue<Vector2>().x;
+        // movimenta o player para esquerda e direita a partir da entrada do usu�rio
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+        // mant�m o player dentro dos limites do jogo (eixo x)
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.y);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.y);
+        }
+        // dispara comida ao pressionar barra de espa�o
+        if (fireAction.WasPerformedThisFrame())
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+        if(pauseAction.WasPerformedThisFrame())
+        {
+            InputActions.FindActionMap("UI").Enable();
+            InputActions.FindActionMap("Player").Disable();
+            Debug.Log("esc");
+        } else if(despauseAction.WasPerformedThisFrame())
+        {
+            InputActions.FindActionMap("UI").Disable();
+            InputActions.FindActionMap("Player").Enable();
+            Debug.Log("UI");
+        }
+        
+    }
+
+    public void MoveEvent(InputAction.CallbackContext context)
+    {
+        horizontalInput = context.ReadValue<Vector2>().x;
+    }
+
+    //public void FireEvent(InputAction.CallbackContext context)
+    //{
+        //if (context.performed)
+        //{
+       //     Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+     //   }
+        //Debug.Log("pizza");
+   // }
+
+    private void OnEnable()
+    {
+        InputActions.FindActionMap("Player").Enable();
+        InputActions.FindActionMap("Player").Enable();
+        
+    }
+
+    private void OnDisable()
+    {
+        InputActions.FindActionMap("Player").Disable();
+        InputActions.FindActionMap("Player").Disable();
+    }
+
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        fireAction = InputSystem.actions.FindAction("Jump");
+        pauseAction = InputSystem.actions.FindAction("Pause");
+        despauseAction = InputSystem.actions.FindAction("PauseUI");
+    } 
+}
